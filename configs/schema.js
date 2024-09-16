2afbd18f717c88eb0a8c985f3e61d438d5cabc15
hardik18-hk19 <hardikagarwal9308@gmail.com>
2024-09-16 23:31:45 +0530

    Corrected schema

--- configs/schema.js
+++ configs/schema.js
@@ -1,10 +1,6 @@
 const { pgTable, serial, text, varchar } = require("drizzle-orm/pg-core");
 
-<<<<<<< HEAD
 export const JsonForms = pgTable("jsonForms", {
-=======
-export const jsonForms = pgTable("jsonForms", {
->>>>>>> aee7a6911333e526408943fa479c7930f2515d08
   id: serial("id").primaryKey(),
   jsonform: text("jsonform").notNull(),
   createdBy: varchar("createdBy").notNull(),
